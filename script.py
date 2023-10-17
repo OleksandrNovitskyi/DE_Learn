08f27a9a9e8218e247a8e31d375bf7a9e1967d3a
OleksandrNovitskyi <novitskyi.o.s@gmail.com>
2023-10-17 11:25:40 +0300

    Connect to DB

---
+++ script.py
@@ -0,0 +1,18 @@
+import pyodbc 
+import pandas as pd
+
+# cnxn = pyodbc.connect('DRIVER={SQL Server};Server=WIN-2L0QJEVGRLV;Database=Northwind;Port=myport;User ID=myuserid;Password=mypassword')
+cnxn = pyodbc.connect('DRIVER={SQL Server};Server=WIN-2L0QJEVGRLV;Database=Northwind;Trusted_Connection=yes;')
+
+cursor = cnxn.cursor()
+cursor.execute("SELECT * FROM dbo.Customers;")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+cursor.close()
+
+# df = pd.read_sql("SELECT * FROM dbo.Customers", cnxn)
+# print(df)
+
+cnxn.close()
+
